5eb256e0dd6ffbbf478a053d8eeb37ead127b973
Vegctrp <kem.altair@gmail.com>
2019-12-15 19:46:32 -0800

    commentlist作成時に素性処理をするように変更

--- nlplib_pyknp2.py
+++ nlplib_pyknp2.py
@@ -9,10 +9,25 @@ import logging
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
-class Morph():
-    def __init__(self, id, mrph):
-        self.id = id
-        self.knpid = mrph.mrph_id
+##### Morph
+
+def imis_parsing_repname(string):
+    sl = string.split(" ")
+    place = -1
+    for i, info in enumerate(sl):
+        if info[:5]=="代表表記:":
+            place = i
+    sl.pop(place)
+    return " ".join(sl)
+
+Morph_series_columns = ['文番号','文節番号', '形態素番号', 'ID','表層','読み','原形','品詞','品詞細分類','活用1','活用2', '意味情報', '代表表記', '代表表記(ひらがな))']
+
+class Morph:
+    def __init__(self, sid, cid, mid, mrph):
+        self.sid = sid # sentence_id
+        self.cid = cid # chunk_id
+        self.mid = mid # morph_id
+        self.id = mrph.mrph_id
         self.midasi = mrph.midasi
         self.yomi = mrph.yomi
         self.genkei = mrph.genkei
@@ -20,72 +35,60 @@ class Morph():
         self.bunrui = mrph.bunrui
         self.katsuyou1 = mrph.katuyou1
         self.katsuyou2 = mrph.katuyou2
-        self.imis = mrph.imis
-        self.repname = mrph.repname
+        self.imis = imis_parsing_repname(mrph.imis)
+        if mrph.repname=="":
+            self.repname = ""
+            self.repname_kana = ""
+        else:
+            self.repname = mrph.repname.split("/")[0]
+            self.repname_kana = mrph.repname.split("/")[1]
 
-class Chunk():
-    def __init__(self, id, bnst):
-        self.morphs = []
-        self.id = id
-        self.knpid = bnst.bnst_id
-        self.dst = bnst.parent_id
-        self.srcs = []
-        self.midasi = "".join(mrph.midasi for mrph in bnst.mrph_list())
-        self.dpndtype = bnst.dpndtype
-        self.fstring = bnst.fstring
+    def make_morph_series_list(self):
+        return [
+            self.sid,            #文番号
+            self.cid,            #文節番号
+            self.mid,            #形態素番号
+            self.id,
+            self.midasi,         #表層
+            self.yomi,           #読み
+            self.genkei,         #原形     
+            self.hinsi,          #品詞
+            self.bunrui,         #品詞細分類
+            self.katsuyou1,      #活用1
+            self.katsuyou2,      #活用2
+            self.imis,           #意味情報
+            self.repname,        #代表表記
+            self.repname_kana    #代表表記(ひらがな)
+        ]
 
+##### Tag
 
-def pyknp_make_commentlist(text, kparser, lines_split=True):
-    if lines_split:
-        text = re.split('[\n\t。 ]', text) #改行または句点で区切り配列化
-        while '' in text:         #空行は削除
-            text.remove('')
-    else:
-        text = [text]
-    
-    comment_list=[] #
-    for index, sentence in enumerate(text):
-        sentence_list = [] ##
-        logging.debug(sentence)
-        result = kparser.parse(sentence)
+Tag_series_columns = ['文番号', '文節番号', '基本句番号', 'ID', '表層', '掛先ID', '係り受けタイプ', '素性']
 
-        for id,bnst in enumerate(result.bnst_list()):
-            sentence_list.append(Chunk(id, bnst))
-        
-        for id,chunk in enumerate(sentence_list):
-            if chunk.dst != -1:
-                sentence_list[chunk.dst].srcs.append(id)
-        
-        for bid, bnst in enumerate(result.bnst_list()):
-            for mid, mrph in enumerate(bnst.mrph_list()):
-                sentence_list[bid].morphs.append(Morph(mid, mrph))
+class Tag:
+    def __init__(self, sid, cid, tid, tag):
+        self.sid = sid
+        self.cid = cid
+        self.tid = tid
+        self.id = tag.tag_id
+        self.midasi = "".join(mrph.midasi for mrph in tag.mrph_list())
+        self.dst = tag.parent_id
+        self.dpndtype = tag.dpndtype
+        self.fstring = tag.fstring
 
-        comment_list.append(sentence_list)
-    return comment_list
+    def make_tag_series_list(self):
+        return [
+            self.sid,
+            self.cid,
+            self.tid,
+            self.id,
+            self.midasi,
+            self.dst,
+            self.dpndtype,
+            self.fstring
+        ]
 
-
-def pyknp_morph2df(comment_list):
-    df = pd.DataFrame(index=[], columns=['文番号','文節番号', '形態素番号','表層','読み','原形','品詞','品詞細分類','活用1','活用2', '意味情報', '代表表記'])
-
-    for sindex,sentence_list in enumerate(comment_list): 
-        for cid, chunk in enumerate(sentence_list):
-            for mid, mrph in enumerate(chunk.morphs):
-                series = pd.Series( [
-                    sindex,          #文番号
-                    cid,             #文節番号
-                    mid,             #形態素番号
-                    mrph.midasi,        #表層
-                    mrph.yomi,          #読み
-                    mrph.genkei,        #原形     
-                    mrph.hinsi,      #品詞
-                    mrph.bunrui,     #品詞細分類
-                    mrph.katsuyou1,  #活用1
-                    mrph.katsuyou2,     #活用2
-                    mrph.imis,          #意味情報
-                    mrph.repname        #代表表記
-                ], index=df.columns)
-                df = df.append(series, ignore_index = True)
-    return df
+##### Chunk
 
 def fst_parsing_NormReprNotation(fstring):
     pattern = r"<正規化代表表記:(.+?)>"
@@ -120,26 +123,108 @@ def fst_parsing_particleCase(fstring):
     pfst = re.sub(pattern, "", fstring)
     return pc, pfst
 
-def pyknp_chunkinfo(comment_list):
-    df = pd.DataFrame(index=[], columns=['文番号','文節番号','見出し','掛先','正規化代表表記','主辞代表表記','助詞の格','素性'])
+Chunk_series_columns = ['文番号','文節番号','ID','見出し','掛先ID', '係り受けタイプ','正規化代表表記','主辞代表表記','助詞の格','素性']
+
+class Chunk:
+    def __init__(self, sid, cid, bnst):
+        self.morphs = []
+        self.tags = []
+        self.sid = sid
+        self.cid = cid
+        self.id = bnst.bnst_id
+        self.dst = bnst.parent_id
+        self.srcs = []
+        self.midasi = "".join(mrph.midasi for mrph in bnst.mrph_list())
+        self.dpndtype = bnst.dpndtype
+        nrn, fstring = fst_parsing_NormReprNotation(bnst.fstring)
+        mrn, fstring = fst_parsing_MainReprNotation(fstring)
+        pc, fstring = fst_parsing_particleCase(fstring)
+        self.nrn = nrn
+        self.mrn = mrn
+        self.pc = pc
+        self.fstring = fstring
+
+    def make_chunk_series_list(self):
+        return [
+            self.sid,             # 文番号
+            self.cid,             # 文節番号
+            self.id,
+            self.midasi,          # 見出し
+            self.dst,             # 係り受け先文節番号
+            self.dpndtype,        # 係り受けタイプ
+            self.nrn,             # 正規化代表表記
+            self.mrn,             # 主辞代表表記
+            self.pc,              # 助詞の格
+            self.fstring          # 残りの素性
+        ]
+    
+
+
+
+def pyknp_make_commentlist(text, kparser, lines_split=True):
+    if lines_split:
+        text = re.split('[\n\t。 ]', text) #改行または句点で区切り配列化
+        while '' in text:         #空行は削除
+            text.remove('')
+    else:
+        text = [text]
+    
+    comment_list=[] #
+    for sentence_id, sentence in enumerate(text):
+        sentence_list = [] ##
+        logging.debug(sentence)
+        result = kparser.parse(sentence)
+
+        for chunk_id,bnst in enumerate(result.bnst_list()):
+            sentence_list.append(Chunk(sentence_id, chunk_id, bnst))
+        
+        for chunk_id,chunk in enumerate(sentence_list):
+            if chunk.dst != -1:
+                sentence_list[chunk.dst].srcs.append(chunk_id)
+        
+        for chunk_id, bnst in enumerate(result.bnst_list()):
+            for tag_id, tag in enumerate(bnst.tag_list()):
+                sentence_list[chunk_id].tags.append(Tag(sentence_id, chunk_id, tag_id, tag))
+        
+        for chunk_id, bnst in enumerate(result.bnst_list()):
+            for morph_id, mrph in enumerate(bnst.mrph_list()):
+                sentence_list[chunk_id].morphs.append(Morph(sentence_id, chunk_id, morph_id, mrph))
+
+        comment_list.append(sentence_list)
+    return comment_list
+
+
+def pyknp_morph2df(comment_list):
+    df = pd.DataFrame(index=[], columns = Morph_series_columns)
+    for sindex,sentence_list in enumerate(comment_list): 
+        for cid, chunk in enumerate(sentence_list):
+            for mid, mrph in enumerate(chunk.morphs):
+                #print("[%d %d %s]%s###%s###"%(cid,mid,mrph.midasi, mrph.imis, mrph.repname))
+                # make series
+                series = pd.Series(mrph.make_morph_series_list(), index=df.columns)
+                df = df.append(series, ignore_index = True)
+    return df
+
+
+def pyknp_chunk2df(comment_list):
+    df = pd.DataFrame(index=[], columns = Chunk_series_columns)
     for sindex, sentence_list in enumerate(comment_list):
         for cid, chunk in enumerate(sentence_list):
-            nrn, fstring = fst_parsing_NormReprNotation(chunk.fstring)
-            mrn, fstring = fst_parsing_MainReprNotation(fstring)
-            pc, fstring = fst_parsing_particleCase(fstring)
-
-            print(sindex, cid, chunk.midasi, fstring)
-            series = pd.Series([
-                sindex,          # 文番号
-                cid,             # 文節番号
-                chunk.midasi,    # 見出し
-                chunk.dst,       # 係り受け先文節番号
-                nrn,             # 正規化代表表記
-                mrn,             # 主辞代表表記
-                pc,              # 助詞の格
-                fstring          # 残りの素性
-            ], index=df.columns)
+            #print(sindex, cid, chunk.midasi, fstring)
+            # make series
+            series = pd.Series(chunk.make_chunk_series_list(), index = df.columns)
             df = df.append(series, ignore_index = True)
+    return df
+
+def pyknp_tag2df(comment_list):
+    df = pd.DataFrame(index=[], columns = Tag_series_columns)
+    for sindex,sentence_list in enumerate(comment_list): 
+        for cid, chunk in enumerate(sentence_list):
+            for tid, tag in enumerate(chunk.tags):
+                #print("[%d %d %s]###%s###"%(cid,tid,tag.midasi, tag.fstring))
+                # make series
+                series = pd.Series(tag.make_tag_series_list(), index=df.columns)
+                df = df.append(series, ignore_index = True)
     return df
 
 
@@ -157,6 +242,8 @@ def pyknp_dependency_visualize(comment_list, withstr=False):
         g=pydot.graph_from_edges(graph,directed=True)
         #g.write_png("result.png")
         display_png(Image(g.create_png()))
+
+
 
 if __name__ == '__main__':
     knp = KNP(option = '-tab -anaphora', jumanpp=True)
